ec7a2271322e8e50c4652072dec3cdb7cc8d929a
Ponganesan-Ramasamy <rponganesan@gmail.com>
2026-05-15 12:51:05 +0530

    Update StripeProperties.java

--- StripeProperties.java
+++ StripeProperties.java
@@ -1,4 +1,4 @@
-package com.brillio.paymentgateway.service.stripe;
+package com.paymentgateway.service.stripe;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
