25a000162112d736969b408f119b47b5f40317e0
Ponganesan-Ramasamy <rponganesan@gmail.com>
2026-05-15 12:50:16 +0530

    Update SecurityConfig.java

--- SecurityConfig.java
+++ SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.brillio.paymentgateway.security;
+package com.paymentgateway.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
